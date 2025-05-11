cc0ed1fc12a48ce285c7aa857da5139e09cfdeb2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-11 02:11:03 -0400

    imported useState and added state variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
 import data from './data';
+import { useState } from 'react';
 
 function App() {
-
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
   console.log(data);
 
 
