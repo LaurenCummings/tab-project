689afe42cbb54c0f5829b7093852758dceef6bf9
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-05-11 02:23:19 -0400

    Started displaying job info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import data from './data';
 import { useState } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 
 function App() {
   const [jobs, setJobs] = useState(data);
@@ -11,12 +12,23 @@ function App() {
   return (
     <section className="section">
       <div className="title">
-        <h2>experience</h2>
+        <h2>Experience</h2>
         <div className="underline"></div>
       </div>
       <div className="jobs-center">
         {/* btn container */}
-        {/* job */}
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return <div key={index} className="job-desc">
+              <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+              <p>{duty}</p>
+            </div>
+          })}
+        </article>
       </div>
     </section>
   )
